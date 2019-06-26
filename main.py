c871b5fadda368dce23f7937f1150cae17fe942f
Robin Bailey <rorgar1@yahoo.com>
2019-06-26 11:49:43 -0500

    restarted all code

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-from flask import Flask, request, redirect, render_template
-import cgi
-import os
-
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<!doctype html>
+<html>
+    <body> 
+        <form action="/username" method = "POST">
+            <label for="user_name">Username: </label>
+            <input id="user_name" type="text" name="user_name" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
+"""
+
+
+
 @app.route("/")
 def index():
-    return render_template('signup.html', title="Signup Form")
+    return form
 
-@app.route("/hello", methods=['POST'])
-def hello():
-    first_name = request.form['first_name']
-    return '<h1>Hello, ' + first_name + '</h1>'
+@app.route ("/username", methods=["POST"])
+def username():
+    user_name = request.form["user_name"]
+    return "<h1> Welcome, " + user_name + "</h1>"
 
 app.run()
